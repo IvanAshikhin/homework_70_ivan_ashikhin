cc9be5593974ae0ae1653ad5efe379e4288f32b7
Ivan <ashikhin1998@gmail.com>
2023-04-18 00:01:51 +0600

    Detail View
    Реализовал детальный просмотр задачи

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from api.view import TaskListView
+from api.view import TaskListView, TaskUpdateView, DetailTaskView
 
 urlpatterns = [
-    # path('createtask', TaskCreateView.as_view(), name='task_create_api'),
+    # path('updatetask', TaskUpdateView.as_view(), name='task_update_api'),
     path('list_task', TaskListView.as_view(), name='task_view_api'),
+    path('list_task/<int:pk>', DetailTaskView.as_view(), name='task_detail_view_api'),
 ]
--- app/api/view.py
+++ app/api/view.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -5,15 +6,15 @@ from api.serializes import TaskSerializer
 from webapp.models import Task
 
 
-# class TaskCreateView(APIView):
-#
-#     def post(self, request, *args, **kwargs):
-#         serializer = TaskSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         else:
-#             return Response(serializer.errors, status=400)
+class TaskUpdateView(APIView):
+
+    def post(self, request, *args, **kwargs):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
 
 
 class TaskListView(APIView):
@@ -24,3 +25,8 @@ class TaskListView(APIView):
         return Response(serializer.data)
 
 
+class DetailTaskView(APIView):
+    def get(self, request, pk):
+        objects = get_object_or_404(Task, pk=pk)
+        serializer = TaskSerializer(objects)
+        return Response(serializer.data)
