12a4e3a67807de13eed0715e5ad240d47fba6c7e
Ivan <ashikhin1998@gmail.com>
2023-04-18 01:05:45 +0600

    Отформатировал Код

--- app/api/urls.py
+++ app/api/urls.py
@@ -4,7 +4,6 @@ from api.view import TaskListView, DetailTaskView, DeleteView, DeleteViewProject
     ProjectUpdateView
 
 urlpatterns = [
-    # path('updatetask', TaskUpdateView.as_view(), name='task_update_api'),
     path('list_task', TaskListView.as_view(), name='task_view_api'),
     path('list_task/<int:pk>', DetailTaskView.as_view(), name='task_detail_view_api'),
     path('list_task/update/<int:pk>', TaskUpdateView.as_view(), name='task_update_view_api'),
